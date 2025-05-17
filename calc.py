3c2835ba437849f1dd3892c0f7710937bda4b1f9
root <root@kali.kali>
2025-05-16 21:11:50 -0400

    Разрешение конфликтов и обновление локального репозитория

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 def sum_numbers(a, b):
     return a + b
 
-# Пример использования
+
 if __name__ == "__main__":
     print("Сумма 5 и 3:", sum_numbers(5, 3))
     print("Сумма 2.5 и 1.1:", sum_numbers(2.5, 1.1))
